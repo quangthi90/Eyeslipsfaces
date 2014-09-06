cf91dfe8e33b6a16194d9af6e6ecad205b2952df
Bommer <quangthi_90@yahoo.com.vn>
2014-09-06 15:55:41 +0700

    - Add file config
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ docs/files/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Eyeslipsfaces/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Eyeslipsfaces/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Eyeslipsfaces/catalog/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Eyeslipsfaces/system/');
+define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Eyeslipsfaces/system/database/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Eyeslipsfaces/catalog/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Eyeslipsfaces/catalog/view/theme/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Eyeslipsfaces/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Eyeslipsfaces/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Eyeslipsfaces/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Eyeslipsfaces/download/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Eyeslipsfaces/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'eyeslipsfaces');
+define('DB_PREFIX', '');
+?>
